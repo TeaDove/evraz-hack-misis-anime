2d6e7a600bdb636f7a46aabb7deb3b8131c2c89f
pibragimov <pibragimov@whoosh.bike>
2023-02-17 18:55:41 +0300

    ref: minor impo

---
+++ backend/app/presentation/kafka/main.py
@@ -0,0 +1,16 @@
+from kafka import KafkaConsumer
+from shared.settings import app_settings
+
+
+def listen_kafka() -> None:
+    consumer = KafkaConsumer(
+        bootstrap_servers=app_settings.kafka_host,
+        security_protocol="SASL_SSL",
+        sasl_mechanism="SCRAM-SHA-512",
+        group_id=app_settings.kafka_consumer_group,
+        enable_auto_commit=False,
+        sasl_plain_username=app_settings.kafka_user,
+        sasl_plain_password=app_settings.kafka_password,
+        ssl_cafile=app_settings.kafka_ca_pem_path,
+    )
+    return consumer
--- backend/app/shared/settings.py
+++ backend/app/shared/settings.py
@@ -6,6 +6,12 @@ class AppSettings(BaseSettings):
     kafka_topic: str
     kafka_user: str
     kafka_password: str
+    kafka_ca_pem_path: str = "CA.pem"
+    kafka_consumer_group: str = "misisAnimeBoys"
+
+    class Config:
+        env_prefix = "misis_"
+        env_file = ".env"
 
 
 app_settings = AppSettings()
