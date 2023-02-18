853b153790e2ac75d2a8e02cab4b5738f6b6828b
pibragimov <pibragimov@whoosh.bike>
2023-02-18 17:58:37 +0300

    feat: add loadtests

--- backend/loadtests/locustfile.py
+++ backend/loadtests/locustfile.py
@@ -4,21 +4,21 @@ from locust import FastHttpUser, task
 
 
 class Backend(FastHttpUser):
-    @task(1)
+    @task(0)
     def ping_ok(self):
         self.client.get("ping")
 
-    @task(10)
+    @task(0)
     def get_exhauster_events(self):
         self.client.get(
-            f"events?exhauster_id={randint(0, 6)}&page={randint(1, 10)}&"
+            f"events?exhauster_id={randint(0, 5)}&page={randint(1, 10)}&"
             f"size={randint(1, 100)}&sort_order={choice(('ASC', 'DESC'))}"
         )
 
-    @task(5)
+    @task(1)
     def get_exhausters(self):
         self.client.get("exhausters")
 
-    @task(10)
+    @task(0)
     def get_exhauster(self):
-        self.client.get(f"exhausters/{randint(0, 6)}")
+        self.client.get(f"exhausters/{randint(0, 5)}")
